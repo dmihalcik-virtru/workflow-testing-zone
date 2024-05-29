be0d3f329c7f57963a44b76da64967e88da672a7
David Mihalcik <dmihalcik@virtru.com>
2024-05-29 16:11:25 -0400

    fix(ci): another attempt to get dep update to work

--- a/ao.go
+++ a/ao.go
@@ -3,6 +3,6 @@ package a
 import "fmt"
 
 func Heyo(name string) string {
-	message := fmt.Sprintf("Hey-o, %v. Welcome from a@v0.0.7!", name)
+	message := fmt.Sprintf("Hey-o, %v. Welcome from a@v0.0.8!", name)
 	return message
 }
