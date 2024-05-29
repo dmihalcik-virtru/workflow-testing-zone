2b1ddea18ad83f298d0c08587c03d551a1697894
David Mihalcik <dmihalcik@virtru.com>
2024-05-29 12:42:32 -0400

    fix(a): Updates version number

--- a/ao.go
+++ a/ao.go
@@ -3,6 +3,6 @@ package a
 import "fmt"
 
 func Heyo(name string) string {
-	message := fmt.Sprintf("Hey-o, %v. Welcome from a@v0.1.0!", name)
+	message := fmt.Sprintf("Hey-o, %v. Welcome from a@v0.0.3!", name)
 	return message
 }
