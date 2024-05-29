ffe2b1163d6d1cfc313bd74cad6499815f9c0213
David Mihalcik <dmihalcik@virtru.com>
2024-05-29 13:01:08 -0400

    fix(a): More investigation

--- a/ao.go
+++ a/ao.go
@@ -3,6 +3,6 @@ package a
 import "fmt"
 
 func Heyo(name string) string {
-	message := fmt.Sprintf("Hey-o, %v. Welcome from a@v0.0.3!", name)
+	message := fmt.Sprintf("Hey-o, %v. Welcome from a@v0.0.4!", name)
 	return message
 }
