3652ebbef4270f4e2a0ebbd8a83827533ddf6169
David Mihalcik <dmihalcik@virtru.com>
2024-05-29 15:50:26 -0400

    fix(ci): Another json error

--- a/ao.go
+++ a/ao.go
@@ -3,6 +3,6 @@ package a
 import "fmt"
 
 func Heyo(name string) string {
-	message := fmt.Sprintf("Hey-o, %v. Welcome from a@v0.0.5!", name)
+	message := fmt.Sprintf("Hey-o, %v. Welcome from a@v0.0.6!", name)
 	return message
 }
