f6d048fd7c675e73fc751f90520f52d0c32cf7d6
David Mihalcik <dmihalcik@virtru.com>
2024-05-30 09:11:59 -0400

    fix(ci): adds pr write permission

--- a/ao.go
+++ a/ao.go
@@ -3,6 +3,6 @@ package a
 import "fmt"
 
 func Heyo(name string) string {
-	message := fmt.Sprintf("Hey-o, %v. Welcome from a@v0.0.12!", name)
+	message := fmt.Sprintf("Hey-o, %v. Welcome from a@v0.0.13!", name)
 	return message
 }
